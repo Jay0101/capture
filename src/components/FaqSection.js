138b9df20eff53bf1ca111ff5e8a6c5721b4523e
Jay0101 <tjr0101@gmail.com>
2020-12-12 23:55:59 -0700

    Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start ?</h4>
+                <div className="answer">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi enim exercitationem hic maxime modi? Voluptates illo ad eos voluptate pariatur. Quisquam vitae perspiciatis soluta fugit quo? Inventore labore ut laudantium.</p>
+
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule ?</h4>
+                <div className="answer">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi enim exercitationem hic maxime modi? Voluptates illo ad eos voluptate pariatur. Quisquam vitae perspiciatis soluta fugit quo? Inventore labore ut laudantium.</p>
+                    
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different payment methods ?</h4>
+                <div className="answer">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi enim exercitationem hic maxime modi? Voluptates illo ad eos voluptate pariatur. Quisquam vitae perspiciatis soluta fugit quo? Inventore labore ut laudantium.</p>
+                    
+                </div>
+            </div>
+            <div className="question">
+                <h4>What Products do you offer ?</h4>
+                <div className="answer">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi enim exercitationem hic maxime modi? Voluptates illo ad eos voluptate pariatur. Quisquam vitae perspiciatis soluta fugit quo? Inventore labore ut laudantium.</p>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection';
+
+const AboutUs = () => {
+    return(
+        <div className="sec">
+            <AboutSection />
+            <ServicesSection />
+            <FaqSection />
+        </div>
+    )
+
+}
+
+export default AboutUs;
